0d7686863c2f253ab76e9c01fc0a3f6daceb8056
Shamus <sushishamus@gmail.com>
2015-05-19 00:56:59 -0700

    navbar collapse on scroll

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 var main = function () {
 
+    var shown = false;
+
     //    var enabled = false;
     //    var scrolled = false;
     //    $('#project-box').hide();
@@ -42,18 +44,123 @@ var main = function () {
 
     $(window).scroll(function () {
 
-//        $('#project-box').each(function () {
-//            var imagePos = $(this).offset().top;
-//
-//            var topOfWindow = $(window).scrollTop();
-//            if (imagePos < topOfWindow + 500) {
-//                $('#project-box').addClass("slideUp2");
-//                //                $('#project-box').show();
-//                //                $('#project-box').removeClass("slideDown");
-//                //                enabled = true;
-//                //                scrolled = true;
-//            }
-//        });
+
+        if ($(window).scrollTop() == 0) {
+            $('.navbar').animate({
+                height: "80px"
+            }, {
+                duration: 300,
+                queue: false
+            });
+
+            $('.logo-class').animate({
+                height: "60px"
+            }, {
+                duration: 300,
+                queue: false
+            });
+
+            $('.logo-class').animate({
+                width: "60px"
+            }, {
+                duration: 300,
+                queue: false
+            });
+
+            $('.logo-class').animate({
+                paddingTop: "9px"
+            }, {
+                duration: 300,
+                queue: false
+            });
+
+            $('.navbar-brand').animate({
+                paddingTop: "30px"
+            }, {
+                duration: 300,
+                queue: false
+            });
+
+            $('.navbar-brand').animate({
+                fontSize: "42px"
+            }, {
+                duration: 300,
+                queue: false
+            });
+
+            $('#navbar a').animate({
+                paddingTop: "30px"
+            }, {
+                duration: 300,
+                queue: false
+            });
+
+            shown = false;
+        } else if ($(window).scrollTop() > 0 && !shown) {
+            $('.navbar').animate({
+                height: "40px"
+            }, {
+                duration: 300,
+                queue: false
+            });
+
+            $('.logo-class').animate({
+                height: "35px"
+            }, {
+                duration: 300,
+                queue: false
+            });
+
+            $('.logo-class').animate({
+                paddingTop: "0px"
+            }, {
+                duration: 300,
+                queue: false
+            });
+
+            $('.logo-class').animate({
+                width: "35px"
+            }, {
+                duration: 300,
+                queue: false
+            });
+
+            $('.navbar-brand').animate({
+                paddingTop: "15px"
+            }, {
+                duration: 300,
+                queue: false
+            });
+
+            $('.navbar-brand').animate({
+                fontSize: "32px"
+            }, {
+                duration: 300,
+                queue: false
+            });
+
+            $('#navbar a').animate({
+                paddingTop: "15px"
+            }, {
+                duration: 300,
+                queue: false
+            });
+
+            shown = true;
+        }
+
+        //        $('#project-box').each(function () {
+        //            var imagePos = $(this).offset().top;
+        //
+        //            var topOfWindow = $(window).scrollTop();
+        //            if (imagePos < topOfWindow + 500) {
+        //                $('#project-box').addClass("slideUp2");
+        //                //                $('#project-box').show();
+        //                //                $('#project-box').removeClass("slideDown");
+        //                //                enabled = true;
+        //                //                scrolled = true;
+        //            }
+        //        });
 
         $('.first-col7').each(function () {
             var imagePos2 = $(this).offset().top;
